0b95ff8bd9cceb39a5956b712d9c69a4cf3fac42
Shun Ueda <hasundue@gmail.com>
2022-01-14 15:47:34 +0900

    Replace execute with cmd

--- denops/vim-pluto/main.ts
+++ denops/vim-pluto/main.ts
@@ -1,5 +1,4 @@
 import { Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts";
-import { execute } from "https://deno.land/x/denops_std@v1.0.0/helper/mod.ts";
 import { ensureString } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
 
 export async function main(denops: Denops): Promise<void> {
@@ -10,8 +9,7 @@ export async function main(denops: Denops): Promise<void> {
         },
     };
 
-    await execute(
-        denops,
+    await denops.cmd(
         `command! -nargs=1 Pluto echomsg denops#request('${denops.name}', 'echo', [<q-args>])`,
     );
 };

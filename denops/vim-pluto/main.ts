59ee7fd447bfd9b841c829206fbb1b8711a9b5a2
Shun Ueda <hasundue@gmail.com>
2022-01-14 15:33:48 +0900

    Command template

--- denops/vim-pluto/main.ts
+++ denops/vim-pluto/main.ts
@@ -1,5 +1,17 @@
 import { Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts";
+import { execute } from "https://deno.land/x/denops_std@v1.0.0/helper/mod.ts";
+import { ensureString } from "https://deno.land/x/unknownutil@v1.0.0/mod.ts";
 
 export async function main(denops: Denops): Promise<void> {
-  console.log("Hello Denops!");
+    denops.dispatcher = {
+        async echo(text: unknown): Promise<unknown> {
+            ensureString(text);
+            return await Promise.resolve(text);
+        },
+    };
+
+    await execute(
+        denops,
+        `command! -nargs=1 Pluto echomsg denops#request('${denops.name}', 'echo', [<q-args>])`,
+    );
 };

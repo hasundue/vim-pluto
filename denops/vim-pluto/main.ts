442de3888a07e57a792e62fd0f1b2bc3d833c8ff
Shun Ueda <hasundue@gmail.com>
2022-01-14 17:15:19 +0900

    Autocmd filetype

--- denops/vim-pluto/main.ts
+++ denops/vim-pluto/main.ts
@@ -1,17 +1,25 @@
-import { Denops } from "https://deno.land/x/denops_std@v1.0.0/mod.ts";
-import { Maze } from "https://deno.land/x/maze_generator@v0.4.0/mod.js";
+import { Denops } from "https://deno.land/x/denops_std@v2.4.0/mod.ts";
+import { Maze } from "https://deno.land/x/maze_generator/mod.js";
 
 export async function main(denops: Denops): Promise<void> {
+
+    async function maze(): Promise<void> {
+        const maze = new Maze({}).generate();
+        const content = maze.getString();
+        await denops.cmd("enew");
+        await denops.call("setline", 1, content.split(/\n/));
+    }
+
     denops.dispatcher = {
-        async maze(): Promise<void> {
-            const maze = new Maze({}).generate();
-            const content = maze.getString();
-            await denops.cmd("enew");
-            await denops.call("setline", 1, content.split(/\n/));
+        async init(): Promise<void> {
+            const line = await denops.call("getline", 1);
+            if (line == "### A Pluto.jl notebook ###") {
+                await maze();
+            };
         },
     };
 
     await denops.cmd(
-        `command! Maze call denops#request('${denops.name}', 'maze', [])`,
+        `autocmd FileType julia call denops#request('${denops.name}', 'init', [])`
     );
 };

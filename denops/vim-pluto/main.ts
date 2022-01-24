8c5956ff8db6ba8046f73370ef9421ec41f990ba
Shun Ueda <hasundue@gmail.com>
2022-01-24 17:04:06 +0900

    Refactor insertCellAbove() and insertCellBelow()

--- denops/vim-pluto/main.ts
+++ denops/vim-pluto/main.ts
@@ -3,44 +3,44 @@ import { ensureNumber, ensureArray, isNumber } from "https://deno.land/x/unknown
 
 export async function main(denops: Denops): Promise<void> {
 
-    async function currentCellRange(): Promise<number[]> {
-        const cursorPosition: unknown = await denops.call("getcurpos");
-        ensureArray(cursorPosition, isNumber);
-
-        const currentLine: number = cursorPosition[1];
-
-        const lastLine: unknown = await denops.call("line", "$");
-        ensureNumber(lastLine);
-
-        const currentCellStart: unknown = await denops.call("search", "# ╔═╡", 'bn');
-        ensureNumber(currentCellStart);
-
-        const nextCellStart: unknown = await denops.call("search", "# ╔═╡", 'n');
-        ensureNumber(nextCellStart);
-
-        if (nextCellStart > currentLine) {
-            return [currentCellStart, nextCellStart - 2];
-        } else {
-            return [currentCellStart, lastLine];
-        }
-    }
-
     async function insertCell(lineNumber: number): Promise<void> {
         const uuid: string = crypto.randomUUID();
 
-        await denops.call("append", lineNumber, ["", "# ╔═╡ " + uuid, ""])
-        await denops.call("cursor", lineNumber + 3, 1);
+        await denops.call("append", lineNumber, ["# ╔═╡ " + uuid, "", ""])
+        await denops.call("cursor", lineNumber + 2, 1);
         await denops.cmd("startinsert");
     }
 
     denops.dispatcher = {
         async insertCellAbove(): Promise<void> {
-            const range: number[] = await currentCellRange();
-            await insertCell(range[0]-2);
+            const cursorPosition: unknown = await denops.call("getcurpos");
+            ensureArray(cursorPosition, isNumber);
+
+            const currentLine: number = cursorPosition[1];
+
+            const currentCellHeader: unknown = await denops.call("search", "# ╔═╡", 'bn');
+            ensureNumber(currentCellHeader);
+
+            if (currentCellHeader == 0 || currentCellHeader > currentLine) {
+                await insertCell(currentLine - 1);
+            } else {
+                await insertCell(currentCellHeader - 1);
+            }
         },
         async insertCellBelow(): Promise<void> {
-            const range: number[] = await currentCellRange();
-            await insertCell(range[1]);
+            const cursorPosition: unknown = await denops.call("getcurpos");
+            ensureArray(cursorPosition, isNumber);
+
+            const currentLine: number = cursorPosition[1];
+
+            const nextCellHeader: unknown = await denops.call("search", "# ╔═╡", 'n');
+            ensureNumber(nextCellHeader);
+
+            if (nextCellHeader == 0 || nextCellHeader < currentLine) { 
+                await insertCell(currentLine);
+            } else {
+                await insertCell(nextCellHeader - 1);
+            }
         },
     };
 };
